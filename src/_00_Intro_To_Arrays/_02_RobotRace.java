3e2fa491bc9242d9a378f6ed71e8501f50b9028f
league <league@imac-29.attlocal.net>
2025-06-07 11:31:08 -0700

    finish the find the hidden button

--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -9,22 +9,41 @@ import org.jointheleague.graphical.robot.Robot;
 public class _02_RobotRace {
     // 1. make a main method
 	public static void main(String[] args) {
-        
-    }
-        // 2. create an array of 5 robots.
-
+		
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		
         // 3. use a for loop to initialize the robots.
-
-        // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
-        // 5. use another for loop to iterate through the array and make each robot move
-        // a random amount less than 50.
-    
-        // 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		for (int i = 0; i < robots.length; i ++) {
+			robots[i] = new Robot();
+			
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			robots[i].setY(500);
+			robots[i].setX(50 + i*100);
+		}
+		
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
         // screen.
-    
-        // 7. declare that robot the winner and throw it a party!
-    
+		boolean chickenDinner = false;
+		while(chickenDinner == false) {
+			
+			// 5. use another for loop to iterate through the array and make each robot move
+	        // a random amount less than 50.
+			Random movement = new Random();
+			for (int j = 0; j < robots.length; j ++) {
+				robots[j].move(movement.nextInt(50));
+				if (robots[j].getY() < 0) {
+					chickenDinner = true;
+					
+					// 7. declare that robot the winner and throw it a party!
+					System.out.println("Unit " + j + " has managed to reach the top!");
+				}
+			}
+		}
+		
+		
+    }
+
         // 8. try different races with different amounts of robots.
     
         // 9. make the robots race around a circular track.
--- src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
+++ src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
@@ -16,8 +16,9 @@ public class _03_FindTheHiddenButton implements ActionListener{
     JPanel panel;
 
     // 1. create an array of JButtons. Don't initialize it yet.
-
+    JButtons[] buttons;
     // 2. create an integer variable called hiddenButton
+    int hiddenButton;
 
     public static void main(String[] args) {
         new _03_FindTheHiddenButton().start();
@@ -29,7 +30,8 @@ public class _03_FindTheHiddenButton implements ActionListener{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 3. Ask the user to enter a positive number and convert it to an int
-
+        String input = JOptionPane.showInputDialog("Please enter in a positive num: ");
+        int intput = Integer.parseInt(input);
         // 4. Initialize the array of JButtons to be the size of the int
         //    created in step 3
 
